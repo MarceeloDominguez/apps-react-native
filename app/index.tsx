6cf590022a092fbde8c5268e64b3570448412dec
marcelo dominguez <marceelodominguez@gmail.com>
2024-12-07 13:12:11 -0300

    start the project

---
+++ app/index.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Button, ButtonGroup, ButtonText } from "@/components/ui/button";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { router } from "expo-router";
+
+export default function MainApp() {
+  return (
+    <SafeAreaView className="bg-zinc-100 flex-1 p-5">
+      <ButtonGroup space="lg">
+        <Button
+          size="md"
+          variant="solid"
+          action="primary"
+          onPress={() => router.push("/(house-booking)")}
+        >
+          <ButtonText>House Booking App</ButtonText>
+        </Button>
+        <Button size="md" variant="solid" action="primary">
+          <ButtonText>Second App</ButtonText>
+        </Button>
+      </ButtonGroup>
+    </SafeAreaView>
+  );
+}
